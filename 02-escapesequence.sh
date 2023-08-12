280d27f5ca7dd870b91ad5035be04db821f553e5
Sarala Muppaneni <sarala.muppaneni@infor.com>
2023-08-12 12:18:00 +0530

    auto push

---
+++ 02-escapesequence.sh
@@ -0,0 +1,13 @@
+#!/bin/bash   
+
+#Shebang Notation - there can only be one interpreter and it has to represnt 1st line of code
+
+echo Welcome to Bash Scripting 
+#\n will print new line 
+#Sarala\nDevOps
+#Sarala
+#DevOps
+
+
+echo -e Line1\nLine2
+
