6349efda91c45383ee5389220dde56cbc249d295
Sarala Muppaneni <sarala.muppaneni@infor.com>
2023-08-13 08:15:55 +0530

    auto push

---
+++ 04-variables.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Used to hold values dynamically 
+ a=10           #a is an integer 
+ b=20           #b is an integer 
+ c=xyz          #c is string 
+
+ #In linux there is no concept of datatypes, by default everythig is string 
+ #If variables have special charecters enclose them in quotes
+ #Using special charecters we can print the value of variable
+ #$ special charecters will be used to print the value of variabe 
+
+echo $a
+echo $b
+echo $c
+
+echo a
+echo b
+echo c
+
+
+
