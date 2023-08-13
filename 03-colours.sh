dc6017762ed72491882e585d394e7f3af03a3dec
Sarala Muppaneni <sarala.muppaneni@infor.com>
2023-08-13 08:08:41 +0530

    auto push

--- 03-colours.sh
+++ 03-colours.sh
@@ -10,3 +10,4 @@ echo -e "\e[35m I am printing in megentha colour \e[0m"
 echo -e "\e[36m I am printing in cyan colour \e[0m"
 
 echo -e "\e[41;33m I am printing  red background colour with yellow foreground colour \e[0m" 
+echo -e "\e[90;31m I am printing grey and red colours \e[0m"
