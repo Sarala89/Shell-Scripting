0a071d030b4f33cd40672ebcae3158547e85ecaa
Sarala Muppaneni <sarala.muppaneni@infor.com>
2023-08-13 15:28:53 +0530

    auto push

---
+++ 05-special-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Special Variables gives speacial properties to your variables 
+
+Rocket_Name = Chandrayan
+
+echo -e "\e[38m Name of the script is $0 \e[0m"
+echo -e "\e[35m Name of recently launched Rocket is $Rocket_Name \e[0m"
