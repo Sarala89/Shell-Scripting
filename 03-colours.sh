e28c6c9a91c1a3a7f7a22a37c20b6a9bbb21943a
Sarala Muppaneni <sarala.muppaneni@infor.com>
2023-08-13 08:00:48 +0530

    auto push

--- 03-colours.sh
+++ 03-colours.sh
@@ -3,3 +3,8 @@
 #echo "\e[colour codeEm your message \e[0m"
 
 echo -e "\e[33m I am printing Yellow colour \e[0m"
+echo -e "\e[32m I am printing in green clour \e[0m"
+echo -e "\e[31m I am printing in Red colour \e[0m"
+echo -e "\e[34m I am printing in blue colour \e[0m"
+echo -e "\e[35m I am printing in megentha colour \e[0m"
+echo -e "\e[36m I am printing in cyan colour \e[0m"
