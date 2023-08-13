297f972cc8d17404f4b42c7fc875ce91771e2516
Sarala Muppaneni <sarala.muppaneni@infor.com>
2023-08-13 16:19:33 +0530

    auto push

---
+++ 07-commands.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#We have 4 trpes of commands 
+#Binary COmmands
+#Aliases
+#Shell-builtin
+#functions - help you eleminate the  writing repeated set of instructions multiple times
+
+#How to declare a function 
+# f(){
+#     echo function 
+# }
+# f 
+
+sample(){
+    echo "Hai this is a Sample Function"
+    echo "The function is completed"
+}
+
+sample
+
+echo "Sample call is compelted"
+
+sample
