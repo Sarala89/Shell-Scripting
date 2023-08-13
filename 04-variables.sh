0588e8d3c5f5344b6958aec76ed7e2290cf3801e
Sarala Muppaneni <sarala.muppaneni@infor.com>
2023-08-13 15:13:12 +0530

    auto push

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,6 +4,7 @@
  a=10           #a is an integer 
  b=20           #b is an integer 
  c=xyz          #c is string 
+ DATE="02AUG23"
 
  #In linux there is no concept of datatypes, by default everythig is string 
  #If variables have special charecters enclose them in quotes
@@ -13,6 +14,7 @@
 echo "printing the values of ${a}"
 echo "printing the value of ${b}"
 echo "printer the value of $c"
+echo " printing todays date $DATE"
 
 echo a
 echo b
