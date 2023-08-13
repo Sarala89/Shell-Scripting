5d8de4e8b56ac541bee610af6f5cf4161b3004af
Sarala Muppaneni <sarala.muppaneni@infor.com>
2023-08-13 15:44:13 +0530

    auto push

--- 05-special-variables.sh
+++ 05-special-variables.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+a=10
+b=20
+c=30
+
+echo "value of a is $a"
+echo "value of b is $b"
+echo "value of c is $c"
+
 #Special Variables gives speacial properties to your variables 
 echo -e "\e[33m Name of the script is $0 \e[0m"
 echo -e "\e[35m Name of recently launched Rocket is $1 \e[0m"
 echo -e "\e[34m Name of popular vehical  is  $2 \e[0m"
 echo -e "\e[32m Name of prime minister is  $3 \e[0m"
+
+echo -e "\e[33m Used Variables are $* \e[0m"
