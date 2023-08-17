f7c74d5e0d29c854a02e09d486949f9d43d36f08
Sarala Muppaneni <sarala.muppaneni@infor.com>
2023-08-17 15:39:55 +0530

    auto push

---
+++ 09-conditions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Helps us execute something when the factor is true, if not it wont execute
+
+# case $var in
+#     opt1) commands.x
+#     opt2) commands.y
+# esac
+
+Action = $1
+
+case $Action in
+    start)
+        echo "Start the payment service"
+        ;;
+    stop)
+        echo "Stop the payment service"
+        ;;
+    restart)
+        echo "Restarting the payment service"
+        ;;
+esac
