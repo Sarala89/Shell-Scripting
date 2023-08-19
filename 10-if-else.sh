1d1fda3a15f9d9dffba6785421116f168b4758c8
Sarala Muppaneni <Sarala.Mupaneni@gmail.com>
2023-08-19 15:13:41 +0530

    Auto push

--- 10-if-else.sh
+++ 10-if-else.sh
@@ -4,7 +4,10 @@ ACTION=$1
 
 if [ "$ACTION" == "Start" ]; then
     echo -e "\e[32m starting the payment service \e[0m "
-    exit
+    exit 0
+else 
+    echo -e "\e[31m valid option is Start only \e[0m"
+    exit 1
 fi
 
 
